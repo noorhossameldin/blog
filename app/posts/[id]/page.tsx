adea18cd48c20d0e4e286fb33a61f87a7df32541
razerazeu <omarhossameldin2016@gmail.com>
2025-06-29 01:35:19 +0300

    fff

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -4,14 +4,13 @@ type Post = {
   body: string;
 };
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
-  const post: Post = await res.json();
-
-  return {
-    title: `Blog ${post.id} Details`,
-    description: post.body.substring(0, 150),
-  };
+export function generateMetadata({ params }: { params: { id: string } }) {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+    .then(res => res.json())
+    .then((post: Post) => ({
+      title: `Blog ${post.id} Details`,
+      description: post.body.substring(0, 150),
+    }));
 }
 
 export default async function PostPage({ params }: { params: { id: string } }) {
